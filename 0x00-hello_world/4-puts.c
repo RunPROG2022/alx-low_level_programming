fc2959efa90ed2371f28a222832bab710031bec1
RunPROG2022 <Sofaniasaraya@gmail.com>
2022-12-12 05:45:38 -0800

    C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -7,6 +8,7 @@
 
 int main(void)
 {
-        	puts("\"Programming is like building a multilingual puzzle");
-				return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
