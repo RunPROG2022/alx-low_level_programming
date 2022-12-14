b761e003e97af724859ea30f436ccb33fbf3a4ac
RunPROG2022 <Sofaniasaraya@gmail.com>
2022-12-14 06:22:54 -0800

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * Return: Success
+ * main - Entry point
+ * Return:0
  */
+
 int main(void)
 {
-	char RunPROG[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(RunPROG[c]);
-	}
-	_putchar('\n');
-	return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
