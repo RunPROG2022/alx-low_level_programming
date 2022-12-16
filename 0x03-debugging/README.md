943d7875641d780db5973c51c628c09aaf67cac0
RunPROG2022 <Sofaniasaraya@gmail.com>
2022-12-16 12:49:10 -0800

    task 0

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging is really challenging
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+}
